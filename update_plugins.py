c8523b473ac0b0813217ad73a4f39eb0736802ad
yangtian03 <yangtian03@kuaishou.com>
2020-08-18 17:35:36 +0800

    add vim-cmake

--- update_plugins.py
+++ update_plugins.py
@@ -57,6 +57,7 @@ vim-ruby https://github.com/vim-ruby/vim-ruby
 typescript-vim https://github.com/leafgarland/typescript-vim
 vim-javascript https://github.com/pangloss/vim-javascript
 vim-python-pep8-indent https://github.com/Vimjas/vim-python-pep8-indent
+vim-cmake https://github.com/vhdirk/vim-cmake
 """.strip()
 
 GITHUB_ZIP = "%s/archive/master.zip"
